5a36878968985ed4bd6b2dab69b8ece0f023b646
David Monaghan <36232635+dmonagh4n@users.noreply.github.com>
2020-11-04 17:54:04 +0000

    Drafting more comprehensive template

--- elasticsearch/ping-elastic-stack.tf
+++ elasticsearch/ping-elastic-stack.tf
@@ -1,44 +1,258 @@
-provider "vsphere" {
-	user = " " 
-	password = " "
-	vsphere_server = " "	
-	# Environment Variable Equivilants
-	# VSPHERE_USER, VSPHERE_PASSWORD and VSPHERE_SERVER	
-	
-	allow_unverified_ssl = true
+variable "vsphere_user" {
+   description = "The vsphere user for terraform"
+   type = string
 }
 
-# DATA_CENTRES
-data "vsphere_datacenter" "dun-bs" {
+variable "vsphere_password" {
+   description = "The vsphere password for terraform"
+   type = string
+}
+
+variable "vsphere_server" {
+   description = "The target vsphere environment"
+   type = string
+}
+
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
   name = "Dundee"
 }
 
-# DATASTORES
-data "vsphere_datastore" "HX-DS-01" {
-  name          = "datastore1"
-  datacenter_id = data.vsphere_datacenter.dun-bs.id
-}
-data "vsphere_datastore" "HX-DS-02" {
-  name          = "datastore2"
-  datacenter_id = data.vsphere_datacenter.dun-bs.id
+# ELASTICSEARCH DATASTORES
+data "vsphere_datastore" "datastore02" {
+  name          = "HX-Datastore-02"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# RESOURCE_POOLS
+data "vsphere_datastore" "datastore01" {
+  name          = "HX-Datastore-01"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# ELASTICSEARCH RESOURCE POOLS
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/PING-ELASTIC"
+  name          = "PING-ELASTIC"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# NETWORKS
-data "vsphere_network" "ELASTIC-970" {
-  name          = "ELASTICSEARCH-970"
+# ELASTICSEARCH NETWORKS
+data "vsphere_network" "elasticsearch-970" {
+  name          = "PING-ELASTICSEARCH_PRI-970"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-data "vsphere_network" "LOGSTASH-971" {
-  name          = "LOGSTASH-971"
+
+data "vsphere_network" "logstash-971" {
+  name          = "PING-LOGSTASH_PRI-971"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-data "vsphere_network" "KIBANA-972" {
-  name          = "KIBANA-972"
+
+data "vsphere_network" "kibana-972" {
+  name          = "PING-KIBANA_PRI-972"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+# ELASTIC TEMPLATE
+data "vsphere_virtual_machine" "template" {
+  name          = "ELASTIC_TEMPLATE"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# ELASTICSEARCH VMS
+resource "vsphere_virtual_machine" "DUN-BS-EL-M-01" {
+  name             = "DUN-BS-EL-M-01"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore02.id
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+
+  num_cpus = 4
+  memory   = 32000
+  guest_id = "centos7_64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.elasticsearch-970.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 150
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "DUN-BS-EL-M-01"
+        domain = "ping-ns.com"
+      }
+      network_interface {
+        ipv4_address = "172.29.97.10"
+        ipv4_netmask = 26
+      }
+      ipv4_gateway = "172.29.97.62"
+    }
+  }
+  # cdrom {
+  #    datastore_id = data.vsphere_datastore.datastore01.id
+  #    path         = "ISO/CentOS-8.1.1911-x86_64-dvd1.iso"
+  # }
+}
+
+resource "vsphere_virtual_machine" "DUN-BS-EL-D-01" {
+  name             = "DUN-BS-EL-D-01"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore02.id
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+
+  num_cpus = 4
+  memory   = 32000
+  guest_id = "centos7_64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.elasticsearch-970.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 150
+  }
+
+    clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "DUN-BS-EL-D-01"
+        domain = "ping-ns.com"
+      }
+      network_interface {
+        ipv4_address = "172.29.97.11"
+        ipv4_netmask = 26
+      }
+      ipv4_gateway = "172.29.97.62"
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "DUN-BS-EL-D-02" {
+  name             = "DUN-BS-EL-D-02"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore02.id
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+
+  num_cpus = 4
+  memory   = 32000
+  guest_id = "centos7_64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.elasticsearch-970.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 150
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = "DUN-BS-EL-D-02"
+        domain = "ping-ns.com"
+      }
+      network_interface {
+        ipv4_address = "172.29.97.12"
+        ipv4_netmask = 26
+      }
+      ipv4_gateway = "172.29.97.62"
+    }
+  }
+}
+
+# LOGSTASH VMS
+resource "vsphere_virtual_machine" "DUN-BS-LS-01" {
+  name             = "DUN-BS-LS-01"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore02.id
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+
+  num_cpus = 4
+  memory   = 16000
+  guest_id = "centos7_64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.logstash-971.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 150
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "DUN-BS-LS-01"
+        domain = "ping-ns.com"
+      }
+      network_interface {
+        ipv4_address = "172.29.97.66"
+        ipv4_netmask = 26
+      }
+      ipv4_gateway = "172.29.97.125"
+  }
+}
+}
+
+# KIBANA VMS
+resource "vsphere_virtual_machine" "DUN-BS-KIB-01" {
+  name             = "DUN-BS-KIB-01"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore02.id
+  wait_for_guest_ip_timeout = 0
+  wait_for_guest_net_timeout = 0
+
+  num_cpus = 4
+  memory   = 16000
+  guest_id = "centos7_64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.kibana-972.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 150
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "DUN-BS-LS-01"
+        domain = "ping-ns.com"
+      }
+      network_interface {
+        ipv4_address = "172.29.97.130"
+        ipv4_netmask = 26
+      }
+      ipv4_gateway = "172.29.97.190"
+  }
+ }
+}
+dmo
